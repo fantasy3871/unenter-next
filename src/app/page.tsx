27f7388f23198d5d7cbe7201e4eb9ebd29839797
Fantasy <jackleewith@gmail.com>
2024-09-14 23:43:37 +1000

    fix: change input size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,45 +7,53 @@ export default function Home() {
       <div className="flex-grow p-3">
         <NoiseMap />
       </div>
-      <div className="flex px-10 py-2 bottom-3 gap-10">
+      <div className="flex px-10 py-5 gap-10">
         <Input
           color="primary"
+          size="lg"
           label="Terrain Seed"
           labelPlacement="outside"
           variant="bordered"
+          startContent={<></>}
         />
         <Input
           color="primary"
+          size="lg"
           label="Scale"
           labelPlacement="outside"
           variant="bordered"
         />
         <Input
           color="primary"
+          size="lg"
           label="Style"
           labelPlacement="outside"
           variant="bordered"
         />
         <Input
           color="primary"
+          size="lg"
           label="Frequency"
           labelPlacement="outside"
           variant="bordered"
         />
         <Input
           color="primary"
+          size="lg"
           label="Lacunarity"
           labelPlacement="outside"
           variant="bordered"
         />
         <Input
           color="primary"
+          size="lg"
           label="Octaves"
           labelPlacement="outside"
           variant="bordered"
         />
         <Input
           color="primary"
+          size="lg"
           label="Persistence"
           labelPlacement="outside"
           variant="bordered"
