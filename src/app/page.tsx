9f80746f9065f417434c8b3eae42fef186a46f10
Fantasy <jackleewith@gmail.com>
2024-09-16 23:37:52 +1000

    feat: sync inputs with context value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import NoiseMap from '@/components/NoiseMap/noise-map';
-import NoiseMapProvider from '@/contexts/noise-map';
-import { Input } from '@nextui-org/react';
+'use client';
 
-const labels = [
-  'Scale',
-  'Style',
-  'Frequency',
-  'Lacunarity',
-  'Octaves',
-  'Persistence',
-];
+import NoiseMap from '@/components/NoiseMap/noise-map';
+import NoiseMapProvider, { useNoiseMapContext } from '@/contexts/noise-map';
+import { Input } from '@nextui-org/react';
 
 export default function Home() {
   return (
@@ -19,28 +12,112 @@ export default function Home() {
           <NoiseMap />
         </div>
         <div className="flex flex-row flex-wrap gap-3 md:gap-10 px-5 md:px-10 py-5">
-          <Input
-            color="primary"
-            size="lg"
-            label="Terrain Seed"
-            labelPlacement="outside"
-            variant="bordered"
-            startContent={<></>}
-            className="min-w-16 w-16 md:w-32"
-          />
-          {labels.map((label, i) => (
-            <Input
-              key={`input ${i}`}
-              color="primary"
-              size="lg"
-              label={label}
-              labelPlacement="outside"
-              variant="bordered"
-              className="min-w-16 w-16 md:w-32"
-            />
-          ))}
+          <NoiseMapControls />
         </div>
       </div>
     </NoiseMapProvider>
   );
 }
+
+const NoiseMapControls = () => {
+  const {
+    noiseMapData,
+    setNoiseMapData,
+    tileStyle,
+    setTileStyle,
+    scale,
+    setScale,
+    frequency,
+    setFrequency,
+    lacunarity,
+    setLacunarity,
+    octaves,
+    setOctaves,
+    persistence,
+    setPersistence,
+  } = useNoiseMapContext();
+
+  const handleChange =
+    (setter: React.Dispatch<React.SetStateAction<any>>) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setter(e.target.value);
+    };
+
+  return (
+    <div className="flex flex-row flex-wrap gap-3 md:gap-10 px-5 md:px-10 py-5">
+      <Input
+        color="primary"
+        value={noiseMapData.terrainSeed}
+        size="lg"
+        label="Terrain Seed"
+        labelPlacement="outside"
+        variant="bordered"
+        startContent={<></>}
+        className="min-w-16 w-16 md:w-32"
+        onChange={(e) =>
+          setNoiseMapData({ ...noiseMapData, terrainSeed: e.target.value })
+        }
+      />
+      <Input
+        color="primary"
+        value={scale}
+        size="lg"
+        label="Scale"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setScale)}
+      />
+      <Input
+        color="primary"
+        value={tileStyle}
+        size="lg"
+        label="Style"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setTileStyle)}
+      />
+      <Input
+        color="primary"
+        value={frequency}
+        size="lg"
+        label="Frequency"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setFrequency)}
+      />
+      <Input
+        color="primary"
+        value={lacunarity}
+        size="lg"
+        label="Lacunarity"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setLacunarity)}
+      />
+      <Input
+        color="primary"
+        value={octaves}
+        size="lg"
+        label="Octaves"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setOctaves)}
+      />
+      <Input
+        color="primary"
+        value={persistence}
+        size="lg"
+        label="Persistence"
+        labelPlacement="outside"
+        variant="bordered"
+        className="min-w-16 w-16 md:w-32"
+        onChange={handleChange(setPersistence)}
+      />
+    </div>
+  );
+};
