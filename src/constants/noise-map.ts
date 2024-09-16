c10dda1735554a85aa4591631c6a282148e7b9af
Fantasy <jackleewith@gmail.com>
2024-09-16 23:37:01 +1000

    feat: add constants for noise-map

--- src/constants/noise-map.ts
+++ src/constants/noise-map.ts
@@ -1,5 +1,6 @@
 export const UUID_NIL = '00000000-0000-0000-0000-000000000000';
 
+// Default NoiseMapData
 export const DEFAULT_NOISE_MAP_DATA = {
   uuid: UUID_NIL,
   name: 'Unknown New World',
@@ -14,43 +15,33 @@ export const DEFAULT_NOISE_MAP_DATA = {
   faunaSeed: '9dfb1994-64d8-46f6-a376-9d364d28c4d9',
 };
 
+// Default terrain layers
 export const DEFAULT_TERRAIN_LAYERS = [
-  {
-    name: 'Trench',
-    starts: -1.0,
-    color: '#283C54',
-  },
-  {
-    name: 'Deep Sea',
-    starts: -0.9,
-    color: '#304864',
-  },
-  {
-    name: 'Water',
-    starts: -0.7,
-    color: '#395476',
-  },
-  {
-    name: 'Grassland',
-    starts: 0.2,
-    color: '#4d6d35',
-  },
-  {
-    name: 'Mountain',
-    starts: 0.6,
-    color: '#937347',
-  },
-  {
-    name: 'Peak',
-    starts: 0.9,
-    color: '#5D5141',
-  },
+  { name: 'Trench', starts: -1.0, color: '#283C54' },
+  { name: 'Deep Sea', starts: -0.9, color: '#304864' },
+  { name: 'Water', starts: -0.7, color: '#395476' },
+  { name: 'Grassland', starts: 0.2, color: '#4d6d35' },
+  { name: 'Mountain', starts: 0.6, color: '#937347' },
+  { name: 'Peak', starts: 0.9, color: '#5D5141' },
 ];
 
+// Tile style options
 export const TILE = {
   STYLE: {
     FLAT: 'Flat',
     NOISE: 'Noise',
     TERRAIN: 'Terrain',
   },
+};
+
+export const DEFAULT_NOISE_MAP_CONTEXT = {
+  noiseMapData: DEFAULT_NOISE_MAP_DATA,
+  terrainLayers: DEFAULT_TERRAIN_LAYERS,
+  tileStyle: TILE.STYLE.TERRAIN,
+  scale: '3',
+  frequency: '0.01',
+  lacunarity: '1.001',
+  octaves: '4',
+  persistence: '0.5',
+  tileSize: '12',
 };
