1d503da380b45f1fbc8b53e57d3c2b6c1b50fe6a
fantasy <89033750+fantasy3871@users.noreply.github.com>
2024-09-20 00:01:05 +1000

    feat: add index file for icons

---
+++ src/components/assets/icons/index.tsx
@@ -0,0 +1,21 @@
+import AlertIcon from './alert-icon';
+import CheckIcon from './check-icon';
+import CopyIcon from './copy-icon';
+import DiceIcon1 from './dice-icon-1';
+import DiceIcon2 from './dice-icon-2';
+import DiceIcon3 from './dice-icon-3';
+import DiceIcon4 from './dice-icon-4';
+import DiceIcon5 from './dice-icon-5';
+import DiceIcon6 from './dice-icon-6';
+
+export {
+  AlertIcon,
+  CheckIcon,
+  CopyIcon,
+  DiceIcon1,
+  DiceIcon2,
+  DiceIcon3,
+  DiceIcon4,
+  DiceIcon5,
+  DiceIcon6,
+};
